75a935a2e79cf6f3395bdbfb819fe75bd4617bf9
Hayden (reoccurcat) <reoccurcat@gmail.com>
2021-10-19 20:13:04 -0500

    update

---
+++ commands/admin.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+//const { token } = require('../config.json');
+//const clone = require('git-clone');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('admin')
+		.setDescription('Bot owner exclusive commands (do not work yet)')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('update')
+                .setDescription('Updates the bot')),
+	async execute(interaction) {
+		if (interaction.options.getSubcommand() === 'update') { 
+			return
+        }
+	},
+};
